e432fadb45234725153a6d7cdd27c39782cad644
PAClement <clementpaquentin@hotmail.fr>
2023-04-27 16:30:08 +0200

    Add route for user

--- API/routes/users.js
+++ API/routes/users.js
@@ -3,53 +3,67 @@ const userRouter = express.Router();
 
 module.exports = function (pool) {
 
-    userRouter.get("/users", async function (req, res) {
-
-        let conn;
-
-        try{
-            conn = await pool.getConnection();
-            const rows = await conn.query(`SELECT * FROM user`);
-            res.send(JSON.stringify(rows));
-        }catch (e){
-            res.send(e);
-        } finally {
-            if (conn) await conn.release(); // Libère la connexion après chaque requête
-        }
-    })
-
+    /**
+     * Retrieve one user by is ID
+     * */
     userRouter.get("/users/:id", async function (req, res) {
 
         let conn;
 
-        try{
+        try {
             conn = await pool.getConnection();
             const rows = await conn.query(`SELECT * FROM user WHERE id=${req.params.id}`);
             res.send(JSON.stringify(rows));
-        }catch (e){
+        } catch (e) {
             res.send(e)
         } finally {
             if (conn) await conn.release(); // Libère la connexion après chaque requête
         }
     })
 
-    userRouter.post("/users", async function (req, res) {
+    /**
+     * Route for register
+     * */
+    userRouter.post("/register", async function (req, res) {
 
         let conn;
 
-        try{
+        try {
             conn = await pool.getConnection();
             const bodyUser = req.body;
             const rows = await conn.query("INSERT INTO `user` (`id`, `email`, `password`, `firstname`, `lastname`, `birth_date`, `location`)" +
                 " VALUES (NULL,?,?,?,?,?,?)", Object.values(bodyUser));
 
             res.send(JSON.stringify(rows));
-        }catch (e){
+        } catch (e) {
             res.send(e);
         } finally {
             if (conn) await conn.release(); // Libère la connexion après chaque requête
         }
     })
+
+    /**
+     * Route connexion
+     * */
+    userRouter.post("/connection", async function (req, res) {
+
+        let conn;
+
+        try {
+            conn = await pool.getConnection();
+            const rowCheckUser = await conn.query("SELECT id FROM user WHERE email = ?", req.body.email)
+
+            res.send(JSON.stringify(rowCheckUser.length > 0 ? rowCheckUser : {
+                "status": 201,
+                "message": "user not found"
+            }));
+        } catch (e) {
+            res.send(e);
+        } finally {
+            if (conn) await conn.release(); // Libère la connexion après chaque requête
+        }
+    })
+
 
     return userRouter;
 }
