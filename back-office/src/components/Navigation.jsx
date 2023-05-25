9ec0f5f4e1fb25bd5753434c190a90d38a469990
Olivier ROY <92591741+Olivieroy@users.noreply.github.com>
2023-05-25 11:43:22 +0200

    Add folders SASS and design Sidebar

--- back-office/src/components/Navigation.jsx
+++ back-office/src/components/Navigation.jsx
@@ -1,86 +1,84 @@
-import React from 'react';
-import {NavLink} from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 import Button from "./Button";
 
 const Navigation = () => {
+  return (
+    <>
+      <nav className="sidebar ">
+        <header className="sidebar-header">
+          <img className="sidebar-header-logo" src="img/logo.png" alt="Logo" />
+        </header>
 
-    return (
-        <>
-            <nav className="sidebar ">
-                <header>
-                    <div className="image-text">
-                        <span className="image">
-                            <img className="mr-3" src="img/logo.png" alt="Logo"/>
-                        </span>
-                    </div>
-                </header>
+        <div className="menu-bar">
+          <ul className="menu-bar-menu">
+            <li>
+              <Button name="Ajouter un évènement" type="outlined" color="secondary"  />
+            </li>
+            <li className="nav-link">
+              <NavLink
+                to="/"
+                className={({ isActive }) => (isActive ? "nav-active" : "")}
+              >
+                <i className="bx bx-user icon"></i>
+                <span className="text nav-text">Profil</span>
+              </NavLink>
+            </li>
+            <li className="nav-link">
+              <NavLink
+                to="/gestion_evenement"
+                className={({ isActive }) => (isActive ? "nav-active" : "")}
+              >
+                <i className="bx bx-calendar-check icon"></i>
+                <span className="text nav-text">Gestion évènements</span>
+              </NavLink>
+            </li>
+            <li className="nav-link">
+              <NavLink
+                to="/historique"
+                className={({ isActive }) => (isActive ? "nav-active" : "")}
+              >
+                <i className="bx bxs-time-five icon"></i>
+                <span className="text nav-text">Historique</span>
+              </NavLink>
+            </li>
+            <li className="nav-link">
+              <NavLink
+                to="/offre"
+                className={({ isActive }) => (isActive ? "nav-active" : "")}
+              >
+                <i className="bx bxs-dollar-circle icon"></i>
+                <span className="text nav-text">Créer une offre</span>
+              </NavLink>
+            </li>
+            <li className="nav-link">
+              <NavLink
+                to="/info_client"
+                className={({ isActive }) => (isActive ? "nav-active" : "")}
+              >
+                <i className="bx bxs-info-circle icon"></i>
+                <span className="text nav-text">Info client</span>
+              </NavLink>
+            </li>
+            <li className="nav-link">
+              <NavLink
+                to="/parametre"
+                className={({ isActive }) => (isActive ? "nav-active" : "")}
+              >
+                <i className="bx bxs-cog icon"></i>
+                <span className="text nav-text">Paramètre</span>
+              </NavLink>
+            </li>
+          </ul>
 
-                <div className="menu-bar">
-                    <div className="menu">
-                        <Button name="Ajouter un evenement" icon="bx bx-plus"/>
-                        <ul className="menu-links">
-                            <li className="nav-link">
-                                <NavLink to='/' className={({isActive}) => (isActive ? 'nav-active' : '')}>
-                                    <i className='bx bx-user icon'></i>
-                                    <span className="text nav-text">
-                                        Profil
-                                    </span>
-                                </NavLink>
-                            </li>
-                            <li className="nav-link">
-                                <NavLink to='/gestion_evenement' className={({isActive}) => (isActive ? 'nav-active' : '')}>
-                                    <i className='bx bx-calendar-check icon'></i>
-                                    <span className="text nav-text">
-                                        Gestion évènements
-                                    </span>
-                                </NavLink>
-                            </li>
-                            <li className="nav-link">
-                                <NavLink to='/historique' className={({isActive}) => (isActive ? 'nav-active' : '')}>
-                                    <i className='bx bxs-time-five icon'></i>
-                                    <span className="text nav-text">
-                                        Historique
-                                    </span>
-                                </NavLink>
-                            </li>
-                            <li className="nav-link">
-                                <NavLink to='/offre' className={({isActive}) => (isActive ? 'nav-active' : '')}>
-                                    <i className='bx bxs-dollar-circle icon'></i>
-                                    <span className="text nav-text">
-                                        Créer une offre
-                                    </span>
-                                </NavLink>
-                            </li>
-                            <li className="nav-link">
-                                <NavLink to='/info_client' className={({isActive}) => (isActive ? 'nav-active' : '')}>
-                                    <i className='bx bxs-info-circle icon'></i>
-                                    <span className="text nav-text">
-                                        Info client
-                                    </span>
-                                </NavLink>
-                            </li>
-                            <li className="nav-link">
-                                <NavLink to='/parametre' className={({isActive}) => (isActive ? 'nav-active' : '')}>
-                                    <i className='bx bxs-cog icon'></i>
-                                    <span className="text nav-text">
-                                        Paramètre
-                                    </span>
-                                </NavLink>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="bottom-content">
-                        <li className="">
-                            <a href="#">
-                                <i className='bx bx-log-out icon'></i>
-                                <span className="text nav-text">Deconnexion</span>
-                            </a>
-                        </li>
-                    </div>
-                </div>
-            </nav>
-        </>
-    );
+          <a href="#" className="menu-bar-bottom">
+            <i className="bx bx-log-out icon"></i>
+            <span className="text nav-text">Déconnexion</span>
+          </a>
+        </div>
+      </nav>
+    </>
+  );
 };
 
 export default Navigation;
